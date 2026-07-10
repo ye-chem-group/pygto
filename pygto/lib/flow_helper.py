58d58a99bec335e63f6cfab448e22791a4432474
Hong-Zhou Ye <hzyechem@gmail.com>
2026-07-09 23:13:23 -0400

    Add indent option for logging

--- pygto/lib/flow_helper.py
+++ pygto/lib/flow_helper.py
@@ -19,36 +19,39 @@ class StreamObject:
     verbose = 3
     stdout = sys.stdout
 
-    def log_error(self, msg, verbose=None, space=True):
+    def log_error(self, msg, verbose=None, space=True, indent=0):
         if verbose is None: verbose = self.verbose
         if verbose >= LOG_ERROR:
-            self.write_msg(f'ERROR: {msg}', space)
+            self.write_msg(f'ERROR: {msg}', space, indent)
 
-    def log_warn(self, msg, verbose=None, space=True):
+    def log_warn(self, msg, verbose=None, space=True, indent=0):
         if verbose is None: verbose = self.verbose
         if verbose >= LOG_WARN:
-            self.write_msg(f'WARN: {msg}', space)
+            self.write_msg(f'WARN: {msg}', space, indent)
 
-    def log_note(self, msg, verbose=None, space=False):
+    def log_note(self, msg, verbose=None, space=False, indent=0):
         if verbose is None: verbose = self.verbose
         if verbose >= LOG_NOTE:
-            self.write_msg(msg, space)
+            self.write_msg(msg, space, indent)
 
-    def log_info(self, msg, verbose=None, space=False):
+    def log_info(self, msg, verbose=None, space=False, indent=0):
         if verbose is None: verbose = self.verbose
         if verbose >= LOG_INFO:
-            self.write_msg(msg, space)
+            self.write_msg(msg, space, indent)
 
-    def log_debug(self, msg, verbose=None, space=False):
+    def log_debug(self, msg, verbose=None, space=False, indent=0):
         if verbose is None: verbose = self.verbose
         if verbose >= LOG_DEBUG:
-            self.write_msg(msg, space)
+            self.write_msg(msg, space, indent)
 
-    def write_msg(self, msg, space=False):
+    def write_msg(self, msg, space=False, indent=0):
+        if indent < 0:
+            raise ValueError('negative indent')
+        indent = '' if indent == 0 else '  ' * indent
         if space:
-            self.stdout.write(f'\n{msg}\n' + '\n')
+            self.stdout.write(f'\n{indent}{msg}\n' + '\n')
         else:
-            self.stdout.write(f'{msg}' + '\n')
+            self.stdout.write(f'{indent}{msg}' + '\n')
         self.flush_stdout()
 
     def flush_stdout(self):
